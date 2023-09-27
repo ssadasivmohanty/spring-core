8f262bb28b53739591bd736cb2cb390b5d9118e2
sourabha s mohanty <sourabhasikun20@gmail.com>
2023-09-27 09:55:31 +0530

    Upto ConstructorInjection

---
+++ src/main/java/com/springcore/collections/Test.java
@@ -0,0 +1,13 @@
+package com.springcore.collections;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext context=new ClassPathXmlApplicationContext("cconfig.xml");
+        Emp emp1=(Emp) context.getBean("emp1");
+
+        System.out.println(emp1);
+    }
+}
